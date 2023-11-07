4e820ef261714ac5be0e4680e26b90bc2cf03717
BrianKool <brian831208@gmail.com>
2023-11-07 15:10:14 +1100

    Add product adding page for seller only

--- upload.php
+++ upload.php
@@ -1,17 +1,16 @@
 <?php
 $target_dir = "uploads/";
+//target_file is the path of the image !!
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-echo $target_file;
 
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    // echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
@@ -39,14 +38,50 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 
 // Check if $uploadOk is set to 0 by an error
-// if ($uploadOk == 0) {
-//   echo "Sorry, your file was not uploaded.";
-// // if everything is ok, try to upload file
-// } else {
-//   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-//     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-//   } else {
-//     echo "Sorry, there was an error uploading your file.";
-//   }
-// }
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+//********************************************************/
+//upload data to sql database
+$servername = "localhost";
+    $username = "brian";
+    $password = "4587476";
+    $databsae = "brian";
+
+    //create connection 
+    $conn = new mysqli($servername, $username, $password, $databsae);
+
+    //check connection
+    if ($conn->connect_error) {
+        die("connection failed: " . $conn->connect_error);
+    }
+    echo "Connected Successfully" . "<br>";
+
+    $pname = $_REQUEST["pname"];
+    $pdescription = $_REQUEST["pdescription"];
+    $imgPath = $target_file;
+
+    if ($uploadOk == 1) {
+      $sql = "INSERT INTO product (Pimage, Pdescription, Pname) VALUES ('$imgPath', '$pdescription', '$pname')";
+    }else{
+      echo "Something went wrong with your upload files!<br>";
+    }
+
+    if ($conn->query($sql) === TRUE) {
+      session_start();
+      echo "Your product has been added successfully";
+      echo "<script>setTimeout(\"location.href = './product.html';\",1500);</script>";
+    }else{
+      echo "Error: " . $conn->error;
+    }
+
+
 ?>
