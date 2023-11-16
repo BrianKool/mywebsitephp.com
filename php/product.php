d45783530d5f2e761112620cf4790875ea705035
BrianKool <brian831208@gmail.com>
2023-11-17 00:00:04 +1100

    add seller page with what product they sell

--- php/product.php
+++ php/product.php
@@ -1,58 +1,71 @@
 <?php 
-    include 'find.php';
-    include 'credential.php';
-    $servername = "localhost";
-    $username = $GLOBALS["username"];
-    $password = $GLOBALS["password"];
-    $databsae = "brian";
 
-    //create connection 
-    $conn = new mysqli($servername, $username, $password, $databsae);
 
-    //check connection
-    if ($conn->connect_error) {
-        die("connection failed: " . $conn->connect_error);
-    }
+    function show_product($sellerornot){
 
-    ///object method to fetch object///
-    //----- this method return object 
-        
-    $find_value = FALSE;
+        include 'find.php';
+        include 'credential.php';
+        $servername = "localhost";
+        $username = $GLOBALS["username"];
+        $password = $GLOBALS["password"];
+        $databsae = "brian";
 
-    if (!empty ($_SESSION['product'])){
-        $Pname = $_SESSION['product'];
-        unset($_SESSION['product']);
-        $find_value = TRUE;
-    }
+        //create connection 
+        $conn = new mysqli($servername, $username, $password, $databsae);
 
-    if ($find_value){
-       $sql = "SELECT * FROM product WHERE Pname LIKE '%$Pname%';"; 
-    }else{
-        $sql = "SELECT * FROM product;";
-    }
-    
-    $result = $conn->query($sql);
-
-    if($result = $conn->query($sql)){
-        while($obj = $result->fetch_object()){
-            $Pname = $obj->Pname;
-            $Pimage = $obj->Pimage;
-            $Pdescription = $obj->Pdescription;
-            $Pid = $obj->id;
-            echo "<li>
-                    <a href=/html/item.html/$Pid>
-                    <p>$Pname</p>
-                    <img src='$Pimage' alt='error' width='150' height='120'>
-                    <h4>$Pdescription</h4>
-                    </a>
-                </li>";
-
+        //check connection
+        if ($conn->connect_error) {
+            die("connection failed: " . $conn->connect_error);
         }
-    }   
 
-    $conn->close();
+        ///object method to fetch object///
+        //----- this method return object 
+            
+        $find_value = FALSE;
 
+        if (!empty ($_SESSION['product'])){
+            $Pname = $_SESSION['product'];
+            unset($_SESSION['product']);
+            $find_value = TRUE;
+        }
 
-// <img src='/source/unliked.png' width='30' height='30'>
+        $userid = $_SESSION['userid'];
+        
+        if ($sellerornot == 0){
+            if ($find_value){
+                $sql = "SELECT * FROM product WHERE Pname LIKE '%$Pname%';"; 
+            }else{
+                $sql = "SELECT * FROM product;";
+            }
+        }else{
+            if ($find_value){
+                $sql = "SELECT * FROM product WHERE Pname LIKE '%$Pname%';"; 
+            }else{
+                $sql = "SELECT * FROM `product` WHERE Sellerid = $userid;";
+            }
+        }
+        
+        $result = $conn->query($sql);
+
+        if($result = $conn->query($sql)){
+            while($obj = $result->fetch_object()){
+                $Pname = $obj->Pname;
+                $Pimage = $obj->Pimage;
+                $Pdescription = $obj->Pdescription;
+                $Pid = $obj->id;
+                echo "<li>
+                        <a href=/html/item.html/$Pid>
+                        <p>$Pname</p>
+                        <img src='$Pimage' alt='error' width='150' height='120'>
+                        <h4>$Pdescription</h4>
+                        </a>
+                    </li>";
+
+            }
+        }   
+
+        $conn->close();
+
+    }
 
 ?>
--- php/profile_card.php
+++ php/profile_card.php
@@ -1,12 +1,11 @@
 <?php 
-session_start();
     $user = $_SESSION['user'];
     $userid = $_SESSION['userid'];
     $user_photo = $_SESSION['pp'];
 
     echo "
         <p>Welcome back $user</p>
-        <img src='$user_photo' alt='error' width='100' height='100'>
+        <img src='$user_photo' alt='Where are you beauty' width='100' height='100'>
     ";
 
 
